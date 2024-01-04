64537655eb1c2a546a8bf64af6d03a98fd86f5de
shubhampathak <shubham.pathak926@gmail.com>
2024-01-04 16:32:24 +0530

    highlight and feature component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import React from "react";
-import { Hero, Sales } from "./components";
-import { heroapi,popularsales,topratesales } from "./data/data.js";
-
+import { FlexContent, Hero, Sales } from "./components";
+import {
+  heroapi,
+  popularsales,
+  topratesales,
+  highlight,
+  sneaker,
+} from "./data/data.js";
 
 const App = () => {
   return (
     <main className="flex flex-col gap-16 relative">
-      <Hero heroapi={heroapi}/>
-      <Sales endpoint={popularsales} ifExists/>
-      <Sales endpoint={topratesales}/>
+      <Hero heroapi={heroapi} />
+      <Sales endpoint={popularsales} ifExists />
+      <FlexContent endpoint={highlight} ifExists />
+
+      <Sales endpoint={topratesales} />
+      <FlexContent endpoint={sneaker} />
     </main>
-  )
+  );
 };
 
 export default App;
